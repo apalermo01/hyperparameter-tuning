1e945c8e20e70a5958929f7cb722b809e590be2f
Alex Palermo <apalermo01@yahoo.com>
2022-10-04 22:41:13 -0400

    started adding functionality from raytune

--- scripts/experiment_runner.py
+++ scripts/experiment_runner.py
@@ -6,6 +6,12 @@ from hparam_tuning_project.training.trainer import Trainer
 from hparam_tuning_project.data.datasets import PytorchDataset
 from hparam_tuning_project.utils import initialize_callbacks
 import pytorch_lightning as pl
+from ray import air, tune
+from ray.tune.integration.pytorch_lightning import TuneReportCallback, \
+    TuneReportCheckpointCallback
+from ray.tune.schedulers import ASHAScheduler
+from ray.tune import CLIReporter
+
 
 config_path = "/home/alex/Documents/personal-projects/hyperparameter-tuning/training_configs/"
 config_name = "simple_training.yaml"
@@ -42,20 +48,56 @@ def validate_cfg(cfg):
     return cfg
 
 
-def run_optim(cfg):
+def train_func(cfg):
     ### Single optimization run
+    tune_callback = TuneReportCallback(['val_loss'], on='validation_end')
 
     # for now, try training a single model
     dataset = PytorchDataset(**cfg['training_cfg']['data_cfg'])
     model = Trainer(**cfg['training_cfg'])
     callbacks = initialize_callbacks(cfg['callbacks'])
-    learner = pl.Trainer(**cfg['flags'], callbacks=callbacks)
+    learner = pl.Trainer(**cfg['flags'], callbacks=callbacks.append(tune_callback))
     learner.fit(model, dataset)
+
+
+def run_optim(cfg):
+    cfg['training_cfg']['optimizer_cfg']['args']['lr'] = tune.loguniform(1e-4, 1e-1)
+    cfg['flags']['enable_progress_bar'] = False
+    num_epochs = 5
+    scheduler = ASHAScheduler(
+        max_t=num_epochs,
+        grace_period=1,
+        reduction_factor=2
+    )
+
+    reporter = CLIReporter(
+        metric_columns=['val_loss', 'training_iteration']
+    )
+
+    tune_cfg = tune.TuneConfig(
+        metric='val_loss',
+        mode='min',
+        scheduler=scheduler,
+        num_samples=10,
+    )
+
+    run_config = air.RunConfig(
+        name='test',
+        progress_reporter=reporter
+    )
+    train_fn = tune.with_parameters(train_func)
+
+    tuner = tune.Tuner(train_fn,
+                       tune_config=tune_cfg,
+                       param_space=cfg,
+                       run_config=run_config)
+    results = tuner.fit()
 
 
 def main():
     cfg = load_cfg(config_path + config_name)
     cfg = validate_cfg(cfg)
+
     run_optim(cfg)
 
 
