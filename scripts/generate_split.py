61c53b6121b3701c376da2bc474c79ea2cd562c5
Alex Palermo <apalermo01@yahoo.com>
2022-11-08 09:28:01 -0500

    modified train / val splits to use stratified random sampling along with sanity check

--- scripts/generate_split.py
+++ scripts/generate_split.py
@@ -1,32 +1,70 @@
 from hparam_tuning_project.data.datasets import PytorchDataset
 from hparam_tuning_project.utils import PATHS
 import numpy as np
-from torch.utils.data.dataset import random_split
+# from torch.utils.data.dataset import random_split
+from sklearn.model_selection import train_test_split
 import argparse
+import torch
+from torch.utils.data import Subset
 
+
+def check_classes(dataset, train_idxs, val_idxs, targets):
+    """Print out the percentage of the train and val labels belonging to each unique class in targets
+    """
+    train_dataset = Subset(dataset, train_idxs.astype(int))
+    val_dataset = Subset(dataset, val_idxs.astype(int))
+
+    train_labels = np.array([i[1] for i in train_dataset])
+    val_labels = np.array([i[1] for i in val_dataset])
+
+    for c in torch.unique(targets):
+        # calculate percentage of train indices with this class
+        print(f"% of train dataset with class {c}: {(train_labels[train_labels==c.item()].shape[0]/train_labels.shape[0])*100:.4f}%; " + \
+              f"% of val dataset with class {c}: {(val_labels[val_labels==c.item()].shape[0]/val_labels.shape[0])*100:.4f}%")
+    print('\n\n')
 
 def main():
     args = parse_args()
+
+    # load dataset
     ds = PytorchDataset.dataset_registry[args.dataset_id]
     dataset = ds(root=PATHS['dataset_path'] + args.dataset_id + '/',
                  train=True,
                  download=True)
-
+    
+    # initialize a list of indices representing each row in the dataset
     idxes = np.arange(len(dataset))
 
-    train_size = int(0.8 * len(idxes))
-    val_size = len(idxes) - train_size
+    # do the train test split, stratifying on target classes
+    targets = dataset.targets
+    train_idxs, val_idxs = train_test_split(idxes, train_size=0.8, stratify = targets)
 
-    # main split
-    train_idxs, val_idxs = random_split(idxes, [train_size, val_size])
+    print("="*80)
+    print(f'checking class distributions for full {args.dataset_id} dataset, contains {len(train_idxs)} training samples and {len(val_idxs)} val samples')
+    check_classes(dataset, train_idxs, val_idxs, targets)
+
     np.savetxt(f"./splits/{args.dataset_id}_train.txt", train_idxs)
     np.savetxt(f"./splits/{args.dataset_id}_val.txt", val_idxs)
 
-    # one tenth of usual size
-    throwaway = len(idxes) - (train_size // 10) - (val_size // 10)
-    train_idxs, val_idxs, _ = random_split(idxes, [train_size // 10, val_size // 10, throwaway])
-    np.savetxt(f"./splits/{args.dataset_id}_small_train.txt", train_idxs)
-    np.savetxt(f"./splits/{args.dataset_id}_small_val.txt", val_idxs)
+    # generate splits for subset of original datdaset
+    fracs = [0.75, 0.5, 0.25, 0.1]
+
+    for f in fracs:
+        train_subset_idx, _ = train_test_split(train_idxs, train_size=f, stratify=targets[train_idxs])
+        val_subset_idx, _ = train_test_split(val_idxs, train_size=f, stratify=targets[val_idxs])
+        
+        print("="*80)
+        print(f"checking class distributions for {f*100}% of {args.dataset_id} dataset, contains {len(train_subset_idx)} training samples and {len(val_subset_idx)} val samples")
+        check_classes(dataset, train_subset_idx, val_subset_idx, targets)
+         
+        np.savetxt(f"./splits/{args.dataset_id}_{str(f).replace('.', '_')}_train.txt", train_subset_idx)
+        np.savetxt(f"./splits/{args.dataset_id}_{str(f).replace('.', '_')}_val.txt", val_subset_idx)
+
+    # # one tenth of usual size
+    # throwaway = len(idxes) - (train_size // 10) - (val_size // 10)
+    # train_idxs, val_idxs, _ = random_split(idxes, [train_size // 10, val_size // 10, throwaway])
+    # np.savetxt(f"./splits/{args.dataset_id}_small_train.txt", train_idxs)
+    # np.savetxt(f"./splits/{args.dataset_id}_small_val.txt", val_idxs)
 
 
 def parse_args():
