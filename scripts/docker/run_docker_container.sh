142c9a957c7b54c61a6383e31bfba3bad401f9ea
alex <alex.palermo.45@gmail.com>
2022-12-12 10:36:29 -0500

    fixed typo

--- scripts/docker/run_docker_container.sh
+++ scripts/docker/run_docker_container.sh
@@ -2,15 +2,16 @@
 # runs the docker container, creating a bind mound at
 # the file path specified 
 
-if [ -d "$1" ]; then
-    mkdir $1
+read -p "directory on host machine for bind mount: " hostdir
+if [ -d "$hostdir" ]; then
+    mkdir $hostdir
 fi
 
 docker run \
     -d \
     --name hparam \
     --mount type=bind,\
-            source=$1,\
+            source=$hostdir,\
             destination=/home/hparam_results
     hparam_project \
     tail -f /dev/null
